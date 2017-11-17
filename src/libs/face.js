515ab7329b2431ebe4ef410e3f8a9fafae17ed11
Jordan Reedie <jjreedie@gmail.com>
2017-11-17 14:19:40 -0500

    Face data collection frequency now env var (ms)
     - also, face overlay is an env var

--- src/libs/face.js
+++ src/libs/face.js
@@ -5,7 +5,10 @@ export default function trackFace(app, user, roomname, videoId) {
 
   log("starting to track facial movement!");
 
-  var faceEvents = new Thumos(videoId,'video-overlay', true);
+  var faceEvents = new Thumos(videoId,
+      'video-overlay',
+      process.env.REACT_APP_FACE_OVERLAY,
+      process.env.REACT_APP_FACE_FREQ);
   faceEvents.bind('faceMoving', function (data) {
     app.service('faces').create({
       'participant': user,
