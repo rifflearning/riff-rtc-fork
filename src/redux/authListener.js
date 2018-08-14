df7eae3f9698636513405ab9d2000c7eb11ab8cd
Dan Calacci <dan@dcalacci.net>
2018-08-14 12:44:45 -0400

    auth listener

---
+++ src/redux/authListener.js
@@ -0,0 +1,19 @@
+import app from '../firebase';
+import { loginAnonymously } from './actions/auth.js';
+
+
+export default function (dispatch, authState) {
+
+  app.auth().onAuthStateChanged((user) => {
+    console.log("Auth state changed.");
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    if (user.isAnonymous) {
+      console.log("user is anonymous!", user.uid);
+      dispatch(loginAnonymously(uid));
+    } else {
+      console.log("Have a user", user);
+    }
+  });
+}
+
