d1a39a680794507c0fec3d25afab961bb7c38430
Dan Calacci <dan@dcalacci.net>
2018-08-27 17:54:25 -0400

    actions and cards for meetings

--- src/components/Dashboard/DashboardView.js
+++ src/components/Dashboard/DashboardView.js
@@ -3,12 +3,39 @@ import { withRouter } from 'react-router-dom';
 import styled, { injectGlobal, keyframes } from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const DashboardView = ({user, riffAuthToken}) => (
-  <div class="columns">
-    <div class="column">
-      <p>{riffAuthToken}</p>
-      <p>Hiiii Dashboard</p>
+const MeetingView = ({meeting}) => {
+  console.log("rendering meeting:", meeting)
+  return (
+    <div class="tile is-4 is-primary is-parent">
+      <div class="tile is-child box">
+        <p class="title">{meeting.room}</p>
+        <p >{meeting.startTime}</p>
+      </div>
+    </div>
+  );
+};
 
+const MeetingList = ({fetchMeetingStatus, meetings}) => {
+  let meetingTiles = meetings.map((meeting) => {
+    return (<MeetingView key={meeting._id} meeting={meeting}/>);
+  });
+  if (fetchMeetingStatus == 'loading') {
+    return (<p> Loading...</p>);
+  } else {
+    return (
+      <div class="tile is-ancestor is-vertical">
+        {meetingTiles}
+      </div>
+    );
+  }
+};
+
+const DashboardView = ({user, riffAuthToken, meetings,
+                        fetchMeetingStatus, numMeetings}) => (
+  <div class="columns is-centered has-text-center">
+    <div class="column">
+      <p>Hiiii Dashboard</p>
+      <MeetingList meetings={meetings} fetchMeetingStatus={fetchMeetingStatus}/>
     </div>
   </div>
 );
--- src/redux/actions/dashboard.js
+++ src/redux/actions/dashboard.js
@@ -1,18 +1,28 @@
 import {
-  RIFF_AUTHENTICATE_SUCCESS,
-  RIFF_AUTHENTICATE_FAIL,
-  RIFF_PARTICIPANTS_CHANGED,
-  RIFF_TURN_UPDATE,
-  RIFF_JOIN_ROOM,
-  RIFF_MEETING_ID_UPDATE
+  DASHBOARD_UPDATE_MEETING_LIST,
+  DASHBOARD_FETCH_MEETINGS
 } from '../constants/ActionTypes';
 
 import { app, socket} from "../../riff";
 
+export const updateMeetingList = (meetings) => {
+  return {type: DASHBOARD_FETCH_MEETINGS,
+          status: 'loaded',
+          meetings: meetings};
+};
+
 export const loadRecentMeetings = (uid) => dispatch => {
-  return app.service('participants').find({_id: uid}).then((res) => {
+  dispatch({type: DASHBOARD_FETCH_MEETINGS, status: 'loading'});
+  return app.service('participants').find({query: {_id: uid}}).then((res) => {
     console.log(">>fetched participant:", res);
-    return res;
+    return res.data[0];
+  }).then((participant) => {
+    return participant.meetings;
+  }).then((meetingIds) => {
+    return app.service('meetings').find({query: {_id: meetingIds}});
+  }).then((meetingObjects) => {
+    dispatch(updateMeetingList(meetingObjects));
+    console.log("got meeting objects:", meetingObjects);
   }).catch((err) => {
     console.log("Couldn't retrieve meetings", err);
   });
