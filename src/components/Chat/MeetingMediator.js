1d577d3849df8317a7101d0edcc070d88d398a50
Mike Lippert <mike@rifflearning.com>
2018-08-28 00:22:40 -0400

    update ver of many outdated packages; remove unused d3 package

--- src/components/Chat/MeetingMediator.js
+++ src/components/Chat/MeetingMediator.js
@@ -1,5 +1,4 @@
 import util from 'util';
-import * as d3 from 'd3';
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
