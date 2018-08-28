014e88122dfec3e669e40bd3b679ae1bd91db47c
Dan Calacci <dan@dcalacci.net>
2018-08-27 20:09:37 -0400

    actions and state for meeting statistic data

--- src/components/Dashboard/DashboardView.js
+++ src/components/Dashboard/DashboardView.js
@@ -2,40 +2,67 @@ import React, { Component } from "react";
 import { withRouter } from 'react-router-dom';
 import styled, { injectGlobal, keyframes } from 'styled-components';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
-const MeetingView = ({meeting}) => {
-  console.log("rendering meeting:", meeting)
+const MeetingTabs = styled.div.attrs({
+  className: 'tabs is-boxed is-size-5'
+})`
+overflow-y: scroll;
+max-height: 80vh;
+ul {
+-webkit-flex-direction: column;
+flex-direction: column;
+}
+li+li {
+margin-left: 0
+}
+`;
+
+const MeetingView = ({meeting, handleMeetingClick}) => {
+  console.log("rendering meeting:", meeting);
+  let m = moment(meeting.startTime).format("ha MMM Do");
   return (
-    <div class="tile is-4 is-primary is-parent">
-      <div class="tile is-child box">
-        <p class="title">{meeting.room}</p>
-        <p >{meeting.startTime}</p>
-      </div>
-    </div>
+    <li><a onClick = {(event) => handleMeetingClick(event, meeting)}>
+        <p>{m}</p>
+    </a></li>
   );
 };
 
-const MeetingList = ({fetchMeetingStatus, meetings}) => {
+const MeetingList = ({fetchMeetingStatus, meetings, handleMeetingClick}) => {
   let meetingTiles = meetings.map((meeting) => {
-    return (<MeetingView key={meeting._id} meeting={meeting}/>);
+    return (<MeetingView key={meeting._id} meeting={meeting} handleMeetingClick={handleMeetingClick}/>);
   });
   if (fetchMeetingStatus == 'loading') {
     return (<p> Loading...</p>);
   } else {
     return (
-      <div class="tile is-ancestor is-vertical">
-        {meetingTiles}
-      </div>
+      <MeetingTabs>
+        <ul>
+          {meetingTiles}
+        </ul>
+      </MeetingTabs>
     );
   }
 };
 
 const DashboardView = ({user, riffAuthToken, meetings,
-                        fetchMeetingStatus, numMeetings}) => (
-  <div class="columns is-centered has-text-center">
+                        fetchMeetingStatus, numMeetings,
+                        handleMeetingClick, selectedMeeting,
+                        processedUtterances, statsStatus}) => (
+  <div class="columns has-text-centered">
+    <div class="column is-one-third">
+      <MeetingList meetings={meetings} fetchMeetingStatus={fetchMeetingStatus} handleMeetingClick={handleMeetingClick}/>
+    </div>
     <div class="column">
-      <p>Hiiii Dashboard</p>
-      <MeetingList meetings={meetings} fetchMeetingStatus={fetchMeetingStatus}/>
+      {statsStatus == 'loading' ?
+        <p> Loading...</p>
+          :
+          <div>
+            <p>Meeting content goes here.</p>
+            <p>Showing content for meeting {selectedMeeting._id}</p>
+              <p>like, that it has {processedUtterances.length > 0 ? processedUtterances[0].numUtterances : "N/A"} utterances.</p>
+          </div>
+      }
     </div>
   </div>
 );
