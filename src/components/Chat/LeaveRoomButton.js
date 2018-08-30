735e3930f4bd4be46c9974f99e3a5f877542a367
Dan Calacci <dan@dcalacci.net>
2018-08-30 16:12:17 -0400

    redirects:
    - login -> /riffs
    - leave room -> /riffs
    - log out -> /login

--- src/components/Chat/LeaveRoomButton.js
+++ src/components/Chat/LeaveRoomButton.js
@@ -11,7 +11,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   redirect: () => {
-    dispatch(push("/room"));
+    dispatch(push("/riffs"));
   }
 });
 
