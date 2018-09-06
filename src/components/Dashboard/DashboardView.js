eebbd46969eb23b5f75f423cb8cb6f2b32027f8f
Mike Lippert <mike@rifflearning.com>
2018-09-06 19:16:29 -0400

    fix indentation

--- src/components/Dashboard/DashboardView.js
+++ src/components/Dashboard/DashboardView.js
@@ -175,76 +175,76 @@ const DashboardView = ({user, riffAuthToken, meetings,
           {
             statsStatus === 'loading'
               ? <div>
-              <ScaleLoader color={"#8A6A94"}/>
-              </div>
+                  <ScaleLoader color={"#8A6A94"}/>
+                </div>
               :
-              <React.Fragment>
-              <div class="columns">
+                <React.Fragment>
+                <div class="columns">
                   <div class="column has-text-left">
-                      <h2 class="is-size-3 is-primary">Room: {selectedMeeting.room} </h2>
-                        <h3 class="is-size-4 is-primary">{processedUtterances.length} Attendees </h3>
-                          <h3 class="is-size-4 is-primary">{selectedMeetingDuration} </h3>
-                            <br/>
-                            <h2 class="is-size-3 has-text-weight-semi-bold"> Why Turn-Taking? </h2>
+                    <h2 class="is-size-3 is-primary">Room: {selectedMeeting.room} </h2>
+                    <h3 class="is-size-4 is-primary">{processedUtterances.length} Attendees </h3>
+                    <h3 class="is-size-4 is-primary">{selectedMeetingDuration} </h3>
+                    <br/>
+                    <h2 class="is-size-3 has-text-weight-semi-bold"> Why Turn-Taking? </h2>
 
-                              <p>
-                                  In highly collaborative groups, people tend to have even
-                                    turn-taking in which everyone is speaking and listening to each
-                                      other equally. In hierarchical groups, participants tend to speak
-                                    only in response to the meeting convener or manager, and not to
-                                    each other. When turn-taking is relatively equal, this indicates
-                                    a balanced conversation in which people are likely to have been
-                                    collaborating effectively.
-                                </p>
+                    <p>
+                      In highly collaborative groups, people tend to have even
+                      turn-taking in which everyone is speaking and listening to each
+                      other equally. In hierarchical groups, participants tend to speak
+                      only in response to the meeting convener or manager, and not to
+                      each other. When turn-taking is relatively equal, this indicates
+                      a balanced conversation in which people are likely to have been
+                      collaborating effectively.
+                    </p>
 
-                                  <p>
-                                <span class="has-text-weight-bold">The next two Riff metrics we’ll release </span>are affirmations and
-                                  interruptions, both of which indicate dynamic, high-engagement
-                                  conversations.
-                                    </p>
-                                    <br/>
+                    <p>
+                      <span class="has-text-weight-bold">The next two Riff metrics
+                      we’ll release</span> are affirmations and interruptions, both of
+                      which indicate dynamic, high-engagement conversations.
+                    </p>
+                    <br/>
+                    <ul>
+                      <li>
+                        <strong>Affirmations</strong> are micro-gestures or vocalizations
+                        that signal you're listening, like nodding your head or
+                        saying "Ah ha" or "Uh huh". They signal that listeners
+                        are attentive and engaged, and can indicate a cooperative
+                        relationship between participants.
+                      </li>
 
-                              <ul>
-                                  <li>
-                                      <strong>Affirmations</strong> are micro-gestures or vocalizations that signal
-                                        you're listening, like nodding your head or saying "Ah ha" or "Uh
-                        huh". They signal that listeners are attentive and engaged, and
-                                        can indicate a cooperative relationship between participants.
-                                    </li>
-
-                                    <li>
-                                        <strong>Interruptions</strong> are important for understanding how the group is
-                                          functioning overall.  Interruptions that allow the speaker to
-                                          continue, such as when someone says, "Oh, I see! That's so cool,"
-                                          indicate agreement and enthusiasm. Interruptions, that derail a
-                                          speaker and take over the conversation, suggest a power imbalance
-                                          within the group.
-                                      </li>
-                                </ul>
-
-                                <br/>
-                                <p>
-                                    Visit your My Riffs page often over the next few weeks, and see
-                                      what new insights are there.
-                                  </p>
-                    </div>
-                    <div class="column">
-                        <div class="card has-text-centered is-centered" style={{borderRadius: '5px', maxWidth: '30vw', paddingTop: '0.75rem'}}>
-                            <div class="card-image has-text-centered is-centered">
-                                <TurnChart processedUtterances={processedUtterances} participantId={user.uid}/>
-                              </div>
-                              <div class="card-content">
-                                  <div class="title is-5 has-text-left">Time Spoken</div>
-                                    <div class="content has-text-left is-size-7">
-                                        The graph above represents the distribution of speaking during
-                                          your meeting, which is our turn-taking metric. In turn-taking, we
-                                          measure only active human vocalization, not the pauses in normal
-                                          speech, which is why time spoken is typically less than the
-                                          meeting duration.
-                                      </div>
-                                </div>
-                          </div>
+                      <li>
+                        <strong>Interruptions</strong> are important for understanding how the group is
+                        functioning overall.  Interruptions that allow the speaker to
+                        continue, such as when someone says, "Oh, I see! That's so cool,"
+                        indicate agreement and enthusiasm. Interruptions, that derail a
+                        speaker and take over the conversation, suggest a power imbalance
+                        within the group.
+                      </li>
+                    </ul>
+                    <br/>
+                    <p>
+                      Visit your My Riffs page often over the next few weeks, and see
+                      what new insights are there.
+                    </p>
+                  </div>
+                  <div class="column">
+                    <div class="card has-text-centered is-centered"
+                         style={{borderRadius: '5px', maxWidth: '30vw', paddingTop: '0.75rem'}}>
+                      <div class="card-image has-text-centered is-centered">
+                        <TurnChart processedUtterances={processedUtterances} participantId={user.uid}/>
                       </div>
+                      <div class="card-content">
+                        <div class="title is-5 has-text-left">Time Spoken</div>
+                        <div class="content has-text-left is-size-7">
+                          The graph above represents the distribution of speaking during
+                          your meeting, which is our turn-taking metric. In turn-taking, we
+                          measure only active human vocalization, not the pauses in normal
+                          speech, which is why time spoken is typically less than the
+                          meeting duration.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 </React.Fragment>
               }
